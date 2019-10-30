bad0c81766cfaebb02223f07ba60c7f444a7f5e6
PCHIII <treyhelmer@gmail.com>
2019-10-30 09:35:36 -0400

    fixed chatbox split chat

--- client/src/components/Chatbox/index.js
+++ client/src/components/Chatbox/index.js
@@ -8,27 +8,23 @@ import { FormControl, InputGroup, Button} from 'react-bootstrap';
 import './chatbox.css';
 
 const Chatbox = (props) => (
-    <Card className= 'col-9 p-3 border chat mt-5 mb-4 mx-auto'>
-        <Card.Body className="scroll ml-auto col-8">
+    <Card className= 'col-9 p-3 border chat mt-5 mb-4 mx-auto scroll'>
+<Card.Body className='scroll'>
+        <Card.Text className=" ml-auto col-5">
             <p className='response1 speech-bubble p-4'>
             <strong>John - </strong>Contrary to popular belief, Lorem Ipsum is not simply random text.
-            </p>
+            </p>    
 
-            <p className='response speech-bubble ml-5 p-4'>
+        </Card.Text>
+
+        <Card.Text className="mr-auto col-5">
+         
+            <p className='response speech-bubble-left p-4'>
             <strong>Kate - </strong>Contrary to popular belief, Lorem Ipsum is not simply random text.  
             </p>
-            <p className='response1 speech-bubble p-4'>
-            <strong>John - </strong>Contrary to popular belief, Lorem Ipsum is not simply random text. 
-            </p>
+         
+        </Card.Text>
 
-            <p className='response ml-5 speech-bubble p-4'>
-            <strong>Kate - </strong>Contrary to popular belief, Lorem Ipsum is not simply random text.Contrary to popular belief, Lorem Ipsum is not simply random text.Contrary to popular belief, Lorem Ipsum is not simply random text.  
-            </p>
-            <p className='response1 speech-bubble p-4'>
-            <strong>John - </strong>Contrary to popular belief, Lorem Ipsum is not simply random text. 
-            </p>
-
-          
         </Card.Body>
 
         <Card.Footer fixed="bottom" className="text-muted bg-white border-0 mt-3">
