1cdd2d8bbc5789d2cc4e7c652a9ce31803468459
youngwoo.cho <petshop0907@gmail.com>
2019-10-30 20:43:09 -0400

    removed commented out codes and changed the root url route to use get Main and signin url to get signin page

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import Top from "./components/Top"; // Navbar
 import SignIn from "./pages/SignIn/SignIn";
 import SignUp from "./pages/SignUp/SignUp";
 import Community from "./pages/Community/Community"
 import NoMatch from "./pages/NoMatch/NoMatch";
 
-// import Side from "./Main/Components/Side/index";
-// import CardDeck from 'react-bootstrap/CardDeck';
 
 function App() {
   return (
     <Router>
       <div>
-        {/* <Top /> */}
         <Switch>
-          <Route exact path="/" component={SignIn} />
+          <Route exact path="/" component={Main} />
           <Route exact path="/signup" component={SignUp} />
-          {/* <Route exact path="/main" component={Main} /> */}
+          <Route exact path="/signin" component={SignIn} />
           <Route exact path="/community/:id" component={Community} />
           <Route component={NoMatch} />
         </Switch>
@@ -27,28 +23,5 @@ function App() {
   );
 }
 
-// function App() {
-//   return (
-
-     
-//   <div className="App">
-//  <Top /> 
-//       <CardDeck className="pt-5 mt-5 size mx-auto"> 
-//                 <Side />
-// // {/* top and side above need to be permnent as well as carddeck */}
-
-
-//           <SignIn />
-//          <SignUp /> */}
-//          <Chatrooms />
-//         <Chatbox />
-//         {/* {/* <Sideright />  */}
-//      </CardDeck> 
-//          <Footer />     
-       
-//  </div>
-    
-//   );
-// }
 
 export default App;
