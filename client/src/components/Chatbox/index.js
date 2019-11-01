dfc91a74eec0fe2b1912bccfc573b00ca63e4895
PCHIII <treyhelmer@gmail.com>
2019-10-31 23:16:53 -0400

    added autofocus to textinput on chatbox

--- client/src/components/Chatbox/index.js
+++ client/src/components/Chatbox/index.js
@@ -92,7 +92,7 @@ const Chatbox = (props) => (
         <Card.Footer fixed="bottom" className="text-muted bg-white border-0 mt-0 pt-0">
  <label for="input" className="Input-label mut pl-2">say something</label> <i className="far fa-comment fa-sm mut"></i>
             <InputGroup size="lg" className="mb-0 shadow-sm"> 
-            <FormControl className="bg-light" 
+            <FormControl className="bg-light" autofocus="autofocus"
                 placeholder=""
                 aria-label="Recipient's username"
                 aria-describedby="basic-addon2"
