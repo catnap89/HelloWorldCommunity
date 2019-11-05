e9b5ead611673bdda7f17dcba37d079a88de827f
youngwoo.cho <petshop0907@gmail.com>
2019-11-05 03:03:54 -0500

    routes for authentication complete

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -16,29 +16,6 @@ router.post("/auth/register", (req, res) => {
   }
 
   // Check for existing user
-  // User.findOne({ username })
-  //   .then(user => {
-  //     if(user) return res.status(400).send({ msg: "User already exists"});
-
-  //     // if user does not exist, create a new user
-  //     User.create({ username, email, password })
-  //       .then(newUser => {
-  //         // You dont' want to authenticate the user here since they are new and have just registered
-  //         // Instead, you want to simply sign them into the session (store)
-  //         req.login(newUser, err => {
-  //           if(err) {
-  //             console.log(err);
-  //             return res.status(500).send({ msg: err});
-  //           }
-
-  //           res.send({
-  //             msg: "User created successfully!",
-  //             user: newUser
-  //           });
-  //         });
-  //       });
-  //   });
-
   User.findOne({ username })
     .then(user => {
       if (user) {
@@ -57,11 +34,15 @@ router.post("/auth/register", (req, res) => {
             newUser.password = hash;
             newUser.save()
               .then(user => {
-                req.flash(
-                  "success_msg",
-                  "You are now registered and can log in"
-                );
-                // res.redirect("/auth/login");
+                // req.flash(
+                //   "success_msg",
+                //   "You are now registered and can log in"
+                // );
+                res.send({
+                  msg: "User created successfully!",
+                  user: newUser
+                });
+                // res.redirect("/login");
               })
               .catch(err => console.log(err));
           })
@@ -70,16 +51,14 @@ router.post("/auth/register", (req, res) => {
     })
 })
 
+// This one works
 
 // router.post("/auth/login", (req, res, next) => {
 //   // Custom Passport Callback
 //   passport.authenticate("local", {
 //     successRedirect: '/',
-//     failureRedirect: '/login',
-//     failureFlash: true  
+//     failureRedirect: '/login'
 //     })(req, res, result => {
-//   // passport.authenticate("local-signin")(req, res, result => {
-//     // res.send({ user: req.user, success: 1 });
 //       req.login(req.user, (err) => {
 //         if (err) { 
 //           return res.status(505).send({msg: "Wrong Username or Password"}); 
@@ -87,11 +66,12 @@ router.post("/auth/register", (req, res) => {
 //         console.log(req.user);
 //         res.send({success: true, msg: 'User logged in successfully!', user: req.user});
 //       });
-//     });;
+//     });
 // });
 
 
 // Login
+// Also works
 router.post("/auth/login", (req, res, next) => {
   passport.authenticate("local", {
     successRedirect: "/",
@@ -101,6 +81,7 @@ router.post("/auth/login", (req, res, next) => {
 });
 
 
+
 router.get('/auth/isauth', (req, res) => {
   res.send({user: req.user});
 })
@@ -108,8 +89,7 @@ router.get('/auth/isauth', (req, res) => {
 
 router.get('/auth/logout', (req, res) => {
   req.logout();
-  // res.send({ msg: "Logged out successfully!"});
-  res.redirect("/login");
+  res.send({ msg: "Logged out successfully!"});
 })
 
 
