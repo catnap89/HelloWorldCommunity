985159b368bc64186c1eead734c2410ab8248cd5
youngwoo.cho <petshop0907@gmail.com>
2019-11-05 07:11:50 -0500

    sidebar and chatrooms added

--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
-import Top from "../../components/Top"; // Navbar
-// import CardDeck from 'react-bootstrap/CardDeck';
-// import Side from "../../components/Side";
-// import Chatbox from "../../components/Chatbox";
+import Top from "../../components/Top"; 
+import CardDeck from 'react-bootstrap/CardDeck';
+import Side from "../../components/Side";
+import Chatrooms from "../../components/Chatrooms";
 // import Sideright from "../../components/Sideright";
 
 
@@ -17,7 +17,6 @@ class Main extends Component {
 
   // make a request to the backend and check if the req.user object is not null
   // if it's not null, then send that object back and use it on the frontend
-
   // use request.user if is null (false), then send 403. which is going to cause catch on axios
   // props.history,sometihnfskdnf to sign i
 
@@ -25,32 +24,13 @@ class Main extends Component {
     this.checkUser();
   }
 
-
-  // checkUser = () => {
-  //   axios.get('/auth/isauth')
-  //     .then(res => {
-  //         console.log(res.data);
-  //         this.setState({
-  //           userInfo: res.data.user
-  //         })
-  //         console.log(this.state.userInfo);
-  //       })
-  //     .catch(error => {
-  //       // console.log("error: " + error.response.data.message);
-  //       // this.setState({
-  //       //   errorMessage: error.response.data.message
-  //       console.log(error);
-  //       this.props.history.push("/login");
-  //     })
-  // }
-
   checkUser = () => {
     axios.get('/auth/isauth')
       .then(res => {
         console.log("res: " + res.data.user.username);
         if (res.data.user) {
           return this.setState({
-            userInfo: res.data || {}
+            userInfo: res.data.user || {}
           });
         } else {
           this.props.history.push("/login");
@@ -68,13 +48,11 @@ class Main extends Component {
   render() {
     return (
       <div className="App">
-      <Top />
-        <h1>Main Page</h1>  
-        <br />
-        <br />
-        <br />
-        <button onClick={this.checkUser}>Check User</button>
-        <p>Hello, {this.state.userInfo.username}</p>
+        <Top />
+        <CardDeck className="pt-5 mt-5 size mx-auto"> 
+          <Side />
+          <Chatrooms />
+        </CardDeck> 
       </div>
     );
   }
