ad5532c06e1e83cd223cd808891f34a6bc024f80
PCHIII <treyhelmer@gmail.com>
2019-11-05 15:35:03 -0500

    updated chat card with button and some styling

--- client/src/components/Chatrooms/index.js
+++ client/src/components/Chatrooms/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import  CardDeck  from 'react-bootstrap/CardDeck';
 import Card from 'react-bootstrap/Card';
 import './chatrooms.css';
-
+import Button from 'react-bootstrap/Button';
 const Chatrooms = (props) => (
 
 
@@ -12,37 +12,43 @@ const Chatrooms = (props) => (
     <Card className= "cardchat">
       <Card.Body>
       <Card.Title>Car Talk</Card.Title>
+      <hr />
         <Card.Text>
           Come here to talk about cars.
         </Card.Text>
 
       </Card.Body>
+      <Button className="ml-auto mr-2" variant="link">Join Chat</Button>
       <Card.Footer>
-        <small className="text-muted">Moderator : John Smith</small>
+        <small className="text-muted mr-auto">Moderator : John Smith</small> 
       </Card.Footer>
     </Card>
 
     <Card className= "cardchat">
       <Card.Body>
       <Card.Title>Car Talk</Card.Title>
+      <hr />
         <Card.Text>
           Come here to talk about cars.
         </Card.Text>
 
       </Card.Body>
+      <Button className="ml-auto mr-2" variant="link">Join Chat</Button>
       <Card.Footer>
-        <small className="text-muted">Moderator : John Smith</small>
+        <small className="text-muted">Moderator : John Smith</small>  
       </Card.Footer>
     </Card>
 
     <Card className= "cardchat">
       <Card.Body>
       <Card.Title>Car Talk</Card.Title>
+       <hr />
         <Card.Text>
           Come here to talk about cars.
         </Card.Text>
 
       </Card.Body>
+      <Button className="ml-auto mr-2" variant="link">Join Chat</Button>
       <Card.Footer>
         <small className="text-muted">Moderator : John Smith</small>
       </Card.Footer>
