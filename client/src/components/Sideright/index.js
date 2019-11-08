e5a718a0e46a176fd85e50b22346bd7ae0437438
PCHIII <treyhelmer@gmail.com>
2019-11-08 12:33:16 -0500

    side right links added for members in chat

--- client/src/components/Sideright/index.js
+++ client/src/components/Sideright/index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Card from 'react-bootstrap/Card';
+import { Link } from "react-router-dom";
 import './sideright.css';
 
 const Sideright =(props) => (
@@ -11,8 +12,16 @@ const Sideright =(props) => (
 
     <ListGroup className="flush" variant="flush border-left shadow">
     
-        <ListGroup.Item className='pt-3 p-4 list bg-light'>MEMBER
+        <ListGroup.Item className='pt-3 p-4 bg-light border-0' action variant="bg-light" >MEMBER
         </ListGroup.Item>
+
+        <ul className="memList list-unstyled">
+        <Link to='/'><i className="fas fa-user fa-xs"></i><li className="memItems pb-2">Name</li></Link>
+        <Link to='/'><i className="fas fa-user fa-xs"></i><li className="memItems pb-2">Name2</li></Link>
+        <Link to='/'><i className="fas fa-user fa-xs"></i><li className="memItems pb-2">Name3</li></Link>
+        <Link to='/'><i className="fas fa-user fa-xs"></i><li className="memItems pb-2">Name4</li></Link>
+       
+      </ul>
 
     <ListGroup.Item className="bg-light" action variant="bg-light border-0">
         Primary</ListGroup.Item>
