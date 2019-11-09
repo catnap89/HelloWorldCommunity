f9519a4b2bf43b0c15dccecd0c447704e4a63d3f
PCHIII <treyhelmer@gmail.com>
2019-11-09 18:54:26 -0500

    user button adjustment

--- client/src/components/BanBtn/index.js
+++ client/src/components/BanBtn/index.js
@@ -9,9 +9,12 @@ const BanBtn = props => {
   }
   return (
     <DropdownButton title="" className="ml-1 border-0"  variant="" size="sm" >
-  <Dropdown.Item onClick={props.banUser}><i class="fas fa-ban pr-2"></i>Ban User</Dropdown.Item>
+ <Dropdown.Item onClick={props.banUser}><i class="fas fa-ban pr-2"></i>Ban User</Dropdown.Item>
+ 
   <Dropdown.Item className="text-muted disabled">Send Message</Dropdown.Item>
   <Dropdown.Item className="text-muted disabled">Add Friend</Dropdown.Item>
+ 
+ 
     </DropdownButton>
   );
 }
