e10373bbb1f932d029d40daf35a597f4437143c9
PCHIII <treyhelmer@gmail.com>
2019-11-08 19:35:25 -0500

    fixed cardeck spacing on the new pages

--- client/src/pages/FavoriteCommunity/FavoriteCommunity.js
+++ client/src/pages/FavoriteCommunity/FavoriteCommunity.js
@@ -85,7 +85,7 @@ class FavoriteCommunity extends Component {
           <Top 
             username={this.state.userInfo}
           />
-          <CardDeck className="pt-5 mt-5 size mx-auto"> 
+          <CardDeck className="size mx-auto"> 
             <Side />
             <h1>There are no saved communities</h1>
           </CardDeck> 
