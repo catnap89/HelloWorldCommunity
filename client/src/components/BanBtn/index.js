cecf65914da05b7edbb550c68ed045770b862564
PCHIII <treyhelmer@gmail.com>
2019-11-09 14:53:59 -0500

    adjusted marging on ban button

--- client/src/components/BanBtn/index.js
+++ client/src/components/BanBtn/index.js
@@ -8,7 +8,7 @@ const BanBtn = props => {
     return null;
   }
   return (
-    <Button className="ml-2 border-0" variant="outline-danger" data-toggle="tooltip" title="Ban User" size="sm" onClick={props.banUser}><i className="fas fa-ban pb-1 fa=lg"></i>
+    <Button className="ml-1 border-0" variant="outline-danger" data-toggle="tooltip" title="Ban User" size="sm" onClick={props.banUser}><i className="fas fa-ban pb-1 fa=lg"></i>
     </Button>
   );
 }
