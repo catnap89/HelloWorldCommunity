9f2597142c551945bc91cb8ff4976ebea0661548
PCHIII <treyhelmer@gmail.com>
2019-11-11 23:46:41 -0500

    increase font and icon size in chatbox and sideright

--- client/src/components/BanBtn/index.js
+++ client/src/components/BanBtn/index.js
@@ -6,7 +6,7 @@ import './BanBtn.css';
 const BanBtn = props => {
   if (!props.isAdmin) {
     return (
-    <DropdownButton title={<span><i className="fas fa-user fa-xs userlist"> </i> </span>} className="ml-3 mr-1 border-0"  variant="link-dark"  >
+    <DropdownButton title={<span><i className="fas fa-user fa-sm userlist"> </i> </span>} className="ml-3 mr-1 border-0"  variant="link-dark"  >
 
         <Dropdown.Item className="text-muted disabled">Send Message</Dropdown.Item>
         <Dropdown.Item className="text-muted disabled">Add Friend</Dropdown.Item>
